8c47d43f7cb1c4ee434c27efa651cbbf33a8ab35
Lior Grishin <liorgrishinnew@gmail.com>
2024-06-23 14:10:05 +0300

    removed unessesery  comments

--- server/src/index.ts
+++ server/src/index.ts
@@ -30,25 +30,6 @@ connection.connect((err) => {
 import userRouter from './API/users/userRoutes';
 app.use('/api/users', userRouter);
 
-// app.post('/api/login', (req, res) => {
-//     const {username, password} = req.body;
-
-//     const query = `SELECT * FROM users WHERE username = ? AND password = ?`;
-//     connection.query(query, [username,password], (error, results : RowDataPacket[] ) => {
-//         if(error) {
-//             console.error('Error fetching data:', error);
-//             res.status(500).send('Error fetching data');
-//             return
-//         }
-
-//         if(results.length  > 0) {
-//             res.json({ success: true })
-//         } else {
-//             res.json({ success: false, message: 'Invalid credentials'})
-//         }
-//     })
-// });
-
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
