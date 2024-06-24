fb44483b007d751180b1ec9229fe26bb15045c12
Lior Grishin <liorgrishinnew@gmail.com>
2024-06-24 11:05:42 +0300

    Added Login without logic

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { handleLogin } from "../utils/utils";
 
 const Login: React.FC = () => {
+    const [username, setUsername] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+
+    async function handleLoginSubmitted(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        handleLogin(username, password);
+    }
+
     return (
         <div>
-            Login Page
+            <h1>Login</h1>
+            <form onSubmit={handleLoginSubmitted}>
+                <label htmlFor="username">Username:</label>
+                <input type="text" name="username" id="username" value={username} onChange={(e) => setUsername(e.target.value)}/>
+
+                <label htmlFor="password">Password:</label>
+                <input type="password" name="username" id="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+
+                <button type="submit">Login</button>
+            </form>
         </div>
     )
 }
---
+++ client/src/utils/utils.ts
@@ -0,0 +1,5 @@
+export async function handleLogin(username: string, password: string) {
+    console.log("Form submitted");
+    
+
+}
